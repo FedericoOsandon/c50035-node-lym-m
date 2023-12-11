127ee4f95e6dd54241e7a5fa1d49594db4862e11
defe014@gmail.com <defe014@gmail.com>
2023-12-11 17:31:36 -0300

    listo clase 0 - introducción

---
+++ clase0-introduccion/app.js
@@ -0,0 +1,47 @@
+// console.log('Bienvenidos a Node.js')
+// console.error('Esto es un error')
+// console.table([{id: '312321', name: 'Federico'}, {id: '312321', name: 'Federico el mejor'}])
+// console.warn('Esto es un warning')
+// const persona = {
+//     id: '132',
+//     nombre: 'fede'
+// }
+// console.dir(persona)
+
+// variables -> palabra reservada para crear una variable (var, let y cons)
+
+// var nombre = 'Federico' // alcance global
+// const email = 'f@gmail.com'
+
+// let apellido = 'Osandón'
+// console.log(apellido)
+// console.log(edad)
+
+// tipos de datos
+// string - number - bollean - object - array - func - null y undefined
+
+
+// clasificación
+// tipos primitivos 
+    // string number boolean undefine y null
+// tipos Object
+    // object array func 
+
+// copia por valor de los primitivos
+
+// let valor = 6
+// const numero = 5
+
+// let valor1 = valor
+// valor = 3
+// console.log(valor,valor1)
+
+// const persona = {
+//     nombre: 'Fede'
+// }
+// // la copia es por referencia
+// const persona1 = persona
+
+// persona.nombre = 'Juan'
+
+// console.log(persona, persona1)
